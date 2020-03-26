7c16a7031710954a38ce879ba7232023a9b2e6a5
Cristina Pena <cristinalpena@gmail.com>
2020-03-26 16:34:28 -0600

    Restructure redirect to use history on submit to change url to allow for form validation, move all login form elements to login form render

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import './App.css';
-import map from '../assets/map.png';
 import LoginForm from '../LoginForm/LoginForm';
 import Areas from '../Areas/Areas';
 
@@ -9,16 +8,11 @@ class App extends Component {
   constructor() {
     super();
   }
+
   render() {
     return (
       <main>
-        <div className='login-screen'>
-          <header className='login-header'>
-              <img src= {map} />
-              <h1>VRAD</h1>
-          </header>
-          <Route path='/' exact component={ LoginForm } />
-        </div>
+        <Route path='/' exact component={ LoginForm } />
         <Route path='/areas' exact component={ Areas } />
       </main>
     )
--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './LoginForm.css';
 import Areas from '../Areas/Areas';
+import map from '../assets/map.png';
 
 class LoginForm extends Component {
   constructor() {
     super();
+    this.state = {
+      name: '',
+      email: '',
+      type: ''
+    }
+  }
+
+  handleChange = (event) => {
+    let key = event.target.id || event.currentTarget.value;
+    this.setState({ [key]: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.name === '' || this.state.email === '' || this.state.type === '') {
+      event.preventDefault();
+      console.log("ERROR MESSAGE!")
+    } else {
+      this.props.history.push('/areas')
+    }
   }
 
   render() {
     return (
-      <form className='login-form'>
-        <input type='text' id='name-input' placeholder='NAME'/>
-        <input type='text' id='email-input' placeholder='EMAIL'/>
-        <select>
-          <option value="" disabled selected>TYPE OF TRAVEL </option>
-          <option value='business'>BUSINESS</option>
-          <option value='vacation'>VACATION</option>
-          <option value='other'>OTHER</option>
-        </select>
-        <NavLink to='/areas' className='submit-nav'>
-          <button id='submit-button' type='submit'>
-            SUBMIT
-          </button>
-        </NavLink>
-      </form>
+      <div className='login-screen'>
+        <header className='login-header'>
+            <img src= {map} />
+            <h1>VRAD</h1>
+        </header>
+        <form className='login-form' onSubmit={this.handleSubmit}>
+          <input type='text' id='name' placeholder='NAME' onChange={this.handleChange}/>
+          <input type='text' id='email' placeholder='EMAIL'onChange={this.handleChange}/>
+          <select id='type' onChange={this.handleChange}>
+            <option value="" disabled selected>TYPE OF TRAVEL </option>
+            <option value='business' id='type'>BUSINESS</option>
+            <option value='vacation'>VACATION</option>
+            <option value='other'>OTHER</option>
+          </select>
+            <button id='submit-button' type='submit' onClick={this.handleSubmit}>
+              SUBMIT
+            </button>
+        </form>
+      </div>
     )
   }
 }
