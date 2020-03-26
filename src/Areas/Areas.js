6ef3a4bc8464875e7afcdc5e3ae80f34d64bc7ee
Mandy Alford <mandyalford@gmail.com>
2020-03-25 22:57:25 -0600

    Move fetch request inside Area and combine into 1 method

--- src/Areas/Areas.js
+++ src/Areas/Areas.js
@@ -10,10 +10,24 @@ class Areas extends Component {
     }
   }
 
-  async componentDidMount() {
-    let areaInfo = await aPullAreas();
-    this.setState({areas: areaInfo});
-    console.log(this.state)
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/areas')
+      .then(response => response.json())
+      .then(data => {
+          const promises = data.areas.map(area => {
+            return fetch(`http://localhost:3001${area.details}`)
+              .then(res => res.json())
+              .then(areaDetails => {
+                return {
+                  shortName: area.area,
+                  longName: areaDetails.name,
+                  description: areaDetails.about
+                }
+              })
+          })
+          return Promise.all(promises)
+      })
+    .then(areas => this.setState({ areas }))
   }
 
   render() {
@@ -22,7 +36,11 @@ class Areas extends Component {
     return (
       <div>
       {areas.map(item => {
-        return <Card area = {item.area}/>
+        return <Card
+          shortName={ item.shortName }
+          longName={ item.longName }
+          description={ item.description }
+        />
       })}
 
       </div>
