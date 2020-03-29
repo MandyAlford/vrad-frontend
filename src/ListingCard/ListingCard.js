709bfde2b477d046eae9cdef8c80bc713bc6baaa
Cristina Pena <cristinalpena@gmail.com>
2020-03-29 12:02:36 -0600

    Change listing card to class component in order to have a method attached to see more button

--- src/ListingCard/ListingCard.js
+++ src/ListingCard/ListingCard.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import {  NavLink  } from 'react-router-dom';
 
-const ListingCard = (props) => {
-  return (
-    <div className='listing-card'>
-      <p>I am {props.name} {props.listingId}</p>
-      <button type='listing-button' id={props.listingId}>SEE MORE</button>
-    </div>
-
-  )
+class ListingCard extends Component {
+  constructor(props){
+    super();
+  }
+  
+  render() {
+    return (
+      <div className='listing-card'>
+        <p>I am {this.props.name} {this.props.listingId}</p>
+        <button className='listing-button' id={this.props.listingId}>SEE MORE</button>
+      </div>
+    )
+  }
 }
 
 export default (ListingCard);
