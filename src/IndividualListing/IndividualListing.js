71e36442fc1d9a92ccce4e4d91aaade9ea627589
Cristina Pena <cristinalpena@gmail.com>
2020-03-29 14:44:34 -0600

    Add dynamic individual listing details, no styling yet

--- src/IndividualListing/IndividualListing.js
+++ src/IndividualListing/IndividualListing.js
@@ -4,7 +4,14 @@ class IndividualListing extends Component {
   constructor(props){
     super();
     this.state = {
-      listingDetails: {}
+      name: '',
+      address: '',
+      superhost: '',
+      beds: 0,
+      baths: 0,
+      costPerNight: 0,
+      features: [],
+      area: ''
     }
   }
 
@@ -15,16 +22,34 @@ class IndividualListing extends Component {
     fetch(`http://localhost:3001/api/v1/listings/${listingId}`)
       .then(response => response.json())
       .then(data => listingDetails = data)
-      .then(details => this.setState({listingDetails}))
+      .then(details => this.setState({
+        name: listingDetails.name,
+        address: `${listingDetails.address.street}, Denver, CO ${listingDetails.address.zip}`,
+        superhost: listingDetails.details.superhost,
+        beds: listingDetails.details.beds,
+        baths: listingDetails.details.baths,
+        costPerNight: listingDetails.details.cost_per_night,
+        features: listingDetails.details.features,
+        area: listingDetails.area
+      }))
   }
 
   render () {
     return (
       <div className='individual-listing-main'>
-        <h1>{this.state.listingDetails.name}</h1>
+        <h3 className='listing-name'>{this.state.name}</h3>
+        <h4 className='listing-details-bold'>{this.state.area.toUpperCase()}</h4>
+        <h4 className='listing-details-bold'>ADDRESS:</h4> <p>{this.state.address}</p>
+        <h4 className='listing-details-bold'>SUPERHOST:</h4> <p>{this.state.superhost}</p>
+        <h4 className='listing-details-bold'>BEDS:</h4> <p>{this.state.beds}</p>
+        <h4 className='listing-details-bold'>BATHS:</h4> <p>{this.state.baths}</p>
+        <h4 className='listing-details-bold'>NIGHTLY COST:</h4> <p>{this.state.costPerNight}</p>
+        <h4 className='listing-details-bold'>FEATURES:</h4> <p>{this.state.features.join(", ")}</p>
       </div>
     )
   }
 }
+
+        // <h3 className="listing-address">{this.state.listingDetails.address.street} {this.state.listingDetails.address.zip}</h3>
 
 export default IndividualListing;
