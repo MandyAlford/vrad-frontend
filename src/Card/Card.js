ec326eab2ef1d37540a0a67d4953ba6f0e18c4e4
Cristina Pena <cristinalpena@gmail.com>
2020-03-30 14:31:32 -0600

    Add area images to cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Card.css';
+import caphill from '../assets/caphill.jpg';
+import rino from '../assets/rino.jpg';
+import parkhill from '../assets/parkhill.jpg';
+import lohi from '../assets/lohi.jpg';
+
 
 const Card = (props) => {
+  let images = [rino, caphill];
+  let areaImage = images.find(image => image.includes(props.shortName.toLowerCase().split(" ").join("")));
+
   return (
     <div className='area-card'>
       <h2>{props.shortName}</h2>
       <h3>{props.longName}</h3>
+      <img src= {areaImage} />
       <p>{props.description}</p>
       <button type='area-button' onClick={props.routeToListings} id={props.id}>SEE LISTINGS</button>
     </div>
