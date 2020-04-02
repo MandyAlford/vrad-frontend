fc02d145a984d8a419d4034271a08e44c26ed783
Mandy Alford <mandyalford@gmail.com>
2020-04-02 14:30:23 -0600

    Additional favoriteCard tests

--- src/FavoriteCard/FavoriteCard.test.js
+++ src/FavoriteCard/FavoriteCard.test.js
@@ -63,4 +63,104 @@ describe('FavoriteCard', () => {
     const superhost = getByText('SUPERHOST: Yes');
     expect(superhost).toBeInTheDocument();
   });
+
+  it('Renders with a number of beds', () => {
+    const { getByText } = render(
+      <FavoriteCard details={{
+        name: 'Brand New RiNo Build',
+        address: '2525 Arapahoe St, Denver, CO 80205',
+        superhost: 'Yes',
+        beds: 5,
+        baths: 4,
+        costPerNight: 150,
+        features: ['jacuzzi', 'fireplace'],
+        area: 'rino',
+        id: 404
+      }}
+      updateFavorites={ jest.fn()} />
+    )
+
+    const beds = getByText('BEDS: 5');
+    expect(beds).toBeInTheDocument();
+  });
+
+  it('Renders with a number of baths', () => {
+    const { getByText } = render(
+      <FavoriteCard details={{
+        name: 'Brand New RiNo Build',
+        address: '2525 Arapahoe St, Denver, CO 80205',
+        superhost: 'Yes',
+        beds: 5,
+        baths: 4,
+        costPerNight: 150,
+        features: ['jacuzzi', 'fireplace'],
+        area: 'rino',
+        id: 404
+      }}
+      updateFavorites={ jest.fn()} />
+    )
+
+    const baths = getByText('BATHS: 4');
+    expect(baths).toBeInTheDocument();
+  });
+
+  it('Renders with a cost per night', () => {
+    const { getByText } = render(
+      <FavoriteCard details={{
+        name: 'Brand New RiNo Build',
+        address: '2525 Arapahoe St, Denver, CO 80205',
+        superhost: 'Yes',
+        beds: 5,
+        baths: 4,
+        costPerNight: 150,
+        features: ['jacuzzi', 'fireplace'],
+        area: 'rino',
+        id: 404
+      }}
+      updateFavorites={ jest.fn()} />
+    )
+
+    const costPerNight = getByText('COST: $150');
+    expect(costPerNight).toBeInTheDocument();
+  });
+
+  it('Renders with a list of features', () => {
+    const { getByText } = render(
+      <FavoriteCard details={{
+        name: 'Brand New RiNo Build',
+        address: '2525 Arapahoe St, Denver, CO 80205',
+        superhost: 'Yes',
+        beds: 5,
+        baths: 4,
+        costPerNight: 150,
+        features: ['jacuzzi', 'fireplace'],
+        area: 'rino',
+        id: 404
+      }}
+      updateFavorites={ jest.fn()} />
+    )
+
+    const features = getByText('FEATURES: jacuzzi, fireplace');
+    expect(features).toBeInTheDocument();
+  });
+
+  it('Renders with an area', () => {
+    const { getByText } = render(
+      <FavoriteCard details={{
+        name: 'Brand New RiNo Build',
+        address: '2525 Arapahoe St, Denver, CO 80205',
+        superhost: 'Yes',
+        beds: 5,
+        baths: 4,
+        costPerNight: 150,
+        features: ['jacuzzi', 'fireplace'],
+        area: 'rino',
+        id: 404
+      }}
+      updateFavorites={ jest.fn()} />
+    )
+
+    const area = getByText('RINO');
+    expect(area).toBeInTheDocument();
+  });
 })
